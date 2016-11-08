9ed4edba02a532620e219754203c612bbdfda7e8
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-11-08 19:55:08 +0000

    skip upgrade check

--- install/py.sh
+++ install/py.sh
@@ -6,6 +6,9 @@ set -eux
 COMPONENT="${COMPONENT,,}"
 OMEROPATH=${HOME}/Omero${COMPONENT~}
 
+if [ "$SKIP_UPGRADE_CHECK" = true ]; then
+    ${OMEROPATH}/bin/omero config set omero.upgrades.url ""
+fi
 
 until ${OMEROPATH}/bin/omero login -s $SERVER_HOST -p $SERVER_PORT -u root -w omero ; do
   >&2 echo "OMERO.server is unavailable - sleeping"
